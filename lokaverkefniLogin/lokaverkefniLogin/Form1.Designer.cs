269bc86bd7a65007525b33171d6df5608f52a6d3
Jón Bjarni Ólafsson <jonol248@nemi.tskoli.is>
2016-11-25 09:51:15 +0000

    Client update

--- lokaverkefniLogin/lokaverkefniLogin/Form1.Designer.cs
+++ lokaverkefniLogin/lokaverkefniLogin/Form1.Designer.cs
@@ -37,6 +37,13 @@
             this.emailTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSignupEmail = new System.Windows.Forms.TextBox();
+            this.txtSignupPass = new System.Windows.Forms.TextBox();
+            this.btSignup = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // loginButton
@@ -110,12 +117,79 @@
             this.panel1.Size = new System.Drawing.Size(300, 300);
             this.panel1.TabIndex = 6;
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(339, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Email :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 244);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Password :";
+            // 
+            // txtSignupEmail
+            // 
+            this.txtSignupEmail.Location = new System.Drawing.Point(390, 215);
+            this.txtSignupEmail.Name = "txtSignupEmail";
+            this.txtSignupEmail.Size = new System.Drawing.Size(168, 20);
+            this.txtSignupEmail.TabIndex = 9;
+            // 
+            // txtSignupPass
+            // 
+            this.txtSignupPass.Location = new System.Drawing.Point(390, 244);
+            this.txtSignupPass.Name = "txtSignupPass";
+            this.txtSignupPass.Size = new System.Drawing.Size(168, 20);
+            this.txtSignupPass.TabIndex = 10;
+            // 
+            // btSignup
+            // 
+            this.btSignup.Location = new System.Drawing.Point(390, 270);
+            this.btSignup.Name = "btSignup";
+            this.btSignup.Size = new System.Drawing.Size(168, 23);
+            this.btSignup.TabIndex = 11;
+            this.btSignup.Text = "Sign up!";
+            this.btSignup.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(391, 176);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(167, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Sign up here below!";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(617, 330);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btSignup);
+            this.Controls.Add(this.txtSignupPass);
+            this.Controls.Add(this.txtSignupEmail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.emailTextBox);
@@ -141,6 +215,13 @@
         private System.Windows.Forms.TextBox emailTextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSignupEmail;
+        private System.Windows.Forms.TextBox txtSignupPass;
+        private System.Windows.Forms.Button btSignup;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- lokaverkefniLogin/lokaverkefniLogin/Form2.Designer.cs
+++ lokaverkefniLogin/lokaverkefniLogin/Form2.Designer.cs
@@ -28,21 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtUsers = new System.Windows.Forms.TextBox();
+            this.txtChatbox = new System.Windows.Forms.TextBox();
+            this.txtMessagebox = new System.Windows.Forms.TextBox();
+            this.btSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtUsers
+            // 
+            this.txtUsers.Location = new System.Drawing.Point(13, 13);
+            this.txtUsers.Multiline = true;
+            this.txtUsers.Name = "txtUsers";
+            this.txtUsers.Size = new System.Drawing.Size(133, 408);
+            this.txtUsers.TabIndex = 0;
+            // 
+            // txtChatbox
+            // 
+            this.txtChatbox.Location = new System.Drawing.Point(153, 216);
+            this.txtChatbox.Multiline = true;
+            this.txtChatbox.Name = "txtChatbox";
+            this.txtChatbox.Size = new System.Drawing.Size(429, 178);
+            this.txtChatbox.TabIndex = 1;
+            // 
+            // txtMessagebox
+            // 
+            this.txtMessagebox.Location = new System.Drawing.Point(153, 400);
+            this.txtMessagebox.Name = "txtMessagebox";
+            this.txtMessagebox.Size = new System.Drawing.Size(367, 20);
+            this.txtMessagebox.TabIndex = 2;
+            // 
+            // btSend
+            // 
+            this.btSend.Location = new System.Drawing.Point(526, 400);
+            this.btSend.Name = "btSend";
+            this.btSend.Size = new System.Drawing.Size(56, 23);
+            this.btSend.TabIndex = 3;
+            this.btSend.Text = "Send";
+            this.btSend.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1276, 595);
+            this.ClientSize = new System.Drawing.Size(594, 433);
+            this.Controls.Add(this.btSend);
+            this.Controls.Add(this.txtMessagebox);
+            this.Controls.Add(this.txtChatbox);
+            this.Controls.Add(this.txtUsers);
             this.Name = "Form2";
             this.Text = "Lokaverkefni -Main";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form2_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtUsers;
+        private System.Windows.Forms.TextBox txtChatbox;
+        private System.Windows.Forms.TextBox txtMessagebox;
+        private System.Windows.Forms.Button btSend;
     }
 }
